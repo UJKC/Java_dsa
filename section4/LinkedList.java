53127ac376b77216a878b7e329cd0d258acb9926
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-30 21:44:00 +0530

    Node LinkedList

---
+++ section4/LinkedList.java
@@ -0,0 +1,45 @@
+package section4;
+
+class Node {
+	int data;
+	Node next;
+	
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
+public class LinkedList {
+
+	Node head;
+	Node tail;
+	int length;
+	
+	public LinkedList(int value) {
+		Node newNode = new Node(value);
+		head = newNode;
+		tail = newNode;
+		length = 1;
+	}
+	
+	public void printList() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+	
+	public void getHead() {
+		System.out.println("Head: " + head.data);
+	}
+	
+	public void getTail() {
+		System.out.println("Tail: " + tail.data);
+	}
+	
+	public void getLength() {
+		System.out.println("Length: " + length);
+	}
+}
