9b97492efc66a624a55f0d708ff3d63f4b790e25
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-30 21:45:04 +0530

    print Linked List

---
+++ section4/Main.java
@@ -0,0 +1,13 @@
+package section4;
+import section4.LinkedList;
+import section4.Node;
+
+public class Main {
+	public static void main(String[] args) {
+		LinkedList myLinkedList = new LinkedList(5);
+		myLinkedList.getHead();
+		myLinkedList.getTail();
+		myLinkedList.getLength();
+		myLinkedList.printList();
+	}
+}
